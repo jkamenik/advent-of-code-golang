5011ab995d607e84ab7d76972072bf2360c4b190
John Kamenik <jkamenik@gmail.com>
2020-12-07 14:48:17 -0500

    Solving part two.

--- day-6/parts.go
+++ day-6/parts.go
@@ -8,7 +8,7 @@ import (
 )
 
 func part1(filename string) {
-	items := readInputFile(filename)
+	items := reduceAny(readInputFile(filename))
 
 	count := 0
 	c := &count
@@ -23,9 +23,32 @@ func part1(filename string) {
 }
 
 func part2(filename string) {
+	items := reduceAll(readInputFile(filename))
+
+	count := 0
+	c := &count
+
+	for _, group := range items {
+		count := group["count"]
+
+		for key, answer := range group {
+			if key == "count" {
+				continue
+			}
+
+			if answer == count {
+				fmt.Printf("all members agreed to %s\n", key)
+				*c = *c + 1
+			}
+		}
+	}
+
+	fmt.Printf("%+v\n", items)
+
+	fmt.Printf("Sum: %d\n", count)
 }
 
-func readInputFile(fileName string) []map[string]bool {
+func readInputFile(fileName string) [][]string {
 	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatal(err)
@@ -57,10 +80,10 @@ func readInputFile(fileName string) []map[string]bool {
 		groups = append(groups, lines)
 	}
 
-	return reduce(groups)
+	return groups
 }
 
-func reduce(groups [][]string) []map[string]bool {
+func reduceAny(groups [][]string) []map[string]bool {
 	items := []map[string]bool{}
 
 	for _, g := range groups {
@@ -69,6 +92,34 @@ func reduce(groups [][]string) []map[string]bool {
 		for _, line := range g {
 			for _, letter := range line {
 				item[string(letter)] = true
+				// fmt.Println(string(letter))
+			}
+		}
+
+		items = append(items, item)
+
+		// fmt.Println(len(item))
+	}
+
+	return items
+}
+
+func reduceAll(groups [][]string) []map[string]int {
+	items := []map[string]int{}
+
+	for _, g := range groups {
+		item := map[string]int{}
+
+		for _, line := range g {
+			item["count"] = len(g)
+
+			for _, letter := range line {
+				if x, ok := item[string(letter)]; !ok {
+					item[string(letter)] = 1
+				} else {
+					item[string(letter)] = x + 1
+				}
+
 				// fmt.Println(string(letter))
 			}
 		}
