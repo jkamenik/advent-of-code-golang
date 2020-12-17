5439378a1be9f4160789de1887bc845b0c3796a9
John Kamenik <jkamenik@gmail.com>
2020-12-17 15:35:56 -0500

    Finish part 1.

---
+++ day-15/memory-game.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+type MemoryGame struct {
+	// index is the number spoken, values an array of all the times it was spoken
+	board      map[int][]int
+	turn       int
+	lastSpoken int
+}
+
+func NewMemoryGame(seed []int) *MemoryGame {
+	x := MemoryGame{board: map[int][]int{}, turn: 0}
+
+	for _, value := range seed {
+		x.turn++
+		x.speak(value)
+	}
+
+	return &x
+}
+
+func (game *MemoryGame) speak(value int) int {
+	turns, ok := game.board[value]
+	if !ok {
+		turns = []int{game.turn}
+	} else {
+		turns = append(turns, game.turn)
+	}
+	game.board[value] = turns
+	game.lastSpoken = value
+
+	return value
+}
+
+// Turn takes a turn and returns the value for that turn
+func (game *MemoryGame) Turn() int {
+	game.turn++
+
+	turns, ok := game.board[game.lastSpoken]
+	if !ok || len(turns) == 1 {
+		return game.speak(0)
+	}
+
+	fmt.Printf("Turns: %v\n", turns)
+
+	prev := turns[len(turns)-2]
+	next := turns[len(turns)-1]
+
+	return game.speak(next - prev)
+}
--- day-15/parts.go
+++ day-15/parts.go
@@ -1,11 +1,49 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func part1(filename string) {
 	fmt.Println("--- Part 1 ---")
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		s := strings.Split(line, ",")
+
+		seed := make([]int, 0)
+		for _, i := range s {
+			x, err := strconv.ParseInt(i, 10, 32)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			seed = append(seed, int(x))
+		}
+
+		game := NewMemoryGame(seed)
+
+		fmt.Printf("Starting: %+v\n", game)
+
+		for i := len(seed); i < 2020; i++ {
+			game.Turn()
+
+			fmt.Printf("Turn %d: %+v\n", game.turn, game)
+		}
+	}
 }
 
 func part2(filename string) {
