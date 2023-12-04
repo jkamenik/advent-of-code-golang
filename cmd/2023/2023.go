891cb1e8ad181aa61da85dbab966d2a4f168b434
John Kamenik <johnkamenik@google.com>
2023-12-03 20:42:17 -0500

    making file lookup easier

--- cmd/2023/2023.go
+++ cmd/2023/2023.go
@@ -5,11 +5,11 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/spf13/cobra"
 	"github.com/jkamenik/advent-of-code-golang/input"
+	"github.com/spf13/cobra"
 )
 
-type puzzle func(filename string, file <-chan string)(output string, err error)
+type puzzle func(filename string, file <-chan string) (output string, err error)
 
 var puzzleLookup = map[string]puzzle{}
 
@@ -18,11 +18,11 @@ var twentyTwentyThree = &cobra.Command{
 	Short: "2023 questions",
 	Long: `Run the day and puzzle in question and provides the output.
 
-The input file is assumed to be in ./advent-questions/2023/<day>-<puzzle>.txt.  To change this provide the file path as the last argument
+The input file is assumed to be in ./advent-questions/2023/<day>.txt.  To change this provide the file path as the last argument
 
 Output is provided to the screen.
 `,
-	Args:  cobra.MinimumNArgs(2),
+	Args: cobra.MinimumNArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		lookup := fmt.Sprintf("%s-%s", args[0], args[1])
 		file := ""
@@ -35,7 +35,7 @@ Output is provided to the screen.
 				return err
 			}
 
-			file = filepath.Join(currentDir, "advent-questions", "2023", fmt.Sprintf("%s.txt", lookup))
+			file = filepath.Join(currentDir, "advent-questions", "2023", fmt.Sprintf("%s.txt", args[0]))
 		}
 
 		p, ok := puzzleLookup[lookup]
